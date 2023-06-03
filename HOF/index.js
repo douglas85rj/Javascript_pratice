5d8d142e288f880970ecb3932002d54ddcd057cd
douglas85rj <douglas85rj@gmail.com>
2023-06-03 20:23:56 -0300

    HighOrderFunction

---
+++ HOF/index.js
@@ -0,0 +1,14 @@
+function slowDown(velocity, printer) {
+    let deceleration = 20;
+
+    while (velocity > 0) {
+        printer(velocity);
+        velocity -= deceleration;
+    }
+alert("Nave parada. As portas podem ser abertas.")
+}
+let spaceshipVelocity = 150;
+
+slowDown(spaceshipVelocity, function(velocity) {
+    console.log("Velocidade atual: " + velocity);
+});
